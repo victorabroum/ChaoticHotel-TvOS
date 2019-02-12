87ff554dd0b5cd3123499f1b933864b8cdffbbed
Victor Vasconcelos <victorabroum@gmail.com>
2019-02-12 11:31:08 -0400

    SwiftLint okay

--- App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Controller/GameScene.swift
+++ App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Controller/GameScene.swift
@@ -10,34 +10,34 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-    
+
     var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
-    
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
-    
+    var graphs = [String: GKGraph]()
+
+    private var lastUpdateTime: TimeInterval = 0
+    private var label: SKLabelNode?
+    private var spinnyNode: SKShapeNode?
+
     override func sceneDidLoad() {
         self.lastUpdateTime = 0
     }
-    
+
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
-        
+
         // Initialize _lastUpdateTime if it has not already been
         if (self.lastUpdateTime == 0) {
             self.lastUpdateTime = currentTime
         }
-        
+
         // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
+        let deltaTime = currentTime - self.lastUpdateTime
+
         // Update entities
         for entity in self.entities {
-            entity.update(deltaTime: dt)
+            entity.update(deltaTime: deltaTime)
         }
-        
+
         self.lastUpdateTime = currentTime
     }
 }
