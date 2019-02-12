8f2f2f6f4fc5f9c69bd6306eae436870fa9e92e3
Victor Vasconcelos <victorabroum@gmail.com>
2019-02-12 10:33:52 -0400

    Poject structure update

---
+++ App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Controller/GameScene.swift
@@ -0,0 +1,43 @@
+//
+//  GameScene.swift
+//  ChaoticHotel-TvOS
+//
+//  Created by Victor Vasconcelos on 12/02/19.
+//  Copyright © 2019 FF Studio. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class GameScene: SKScene {
+    
+    var entities = [GKEntity]()
+    var graphs = [String : GKGraph]()
+    
+    private var lastUpdateTime : TimeInterval = 0
+    private var label : SKLabelNode?
+    private var spinnyNode : SKShapeNode?
+    
+    override func sceneDidLoad() {
+        self.lastUpdateTime = 0
+    }
+    
+    override func update(_ currentTime: TimeInterval) {
+        // Called before each frame is rendered
+        
+        // Initialize _lastUpdateTime if it has not already been
+        if (self.lastUpdateTime == 0) {
+            self.lastUpdateTime = currentTime
+        }
+        
+        // Calculate time since last update
+        let dt = currentTime - self.lastUpdateTime
+        
+        // Update entities
+        for entity in self.entities {
+            entity.update(deltaTime: dt)
+        }
+        
+        self.lastUpdateTime = currentTime
+    }
+}
---
+++ App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Controller/GameViewController.swift
@@ -0,0 +1,45 @@
+//
+//  GameViewController.swift
+//  ChaoticHotel-TvOS
+//
+//  Created by Victor Vasconcelos on 12/02/19.
+//  Copyright © 2019 FF Studio. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+import GameplayKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
+        if let scene = GKScene(fileNamed: "GameScene") {
+            
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! GameScene? {
+                
+                // Copy gameplay related content over to the scene
+                sceneNode.entities = scene.entities
+                sceneNode.graphs = scene.graphs
+                
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFill
+                
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
+            }
+        }
+    }
+
+}
