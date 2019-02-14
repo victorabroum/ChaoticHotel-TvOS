9dd443d79d4ff696882d4e34f5170293b06561c9
Renato <renattoolopes@gmail.com>
2019-02-14 14:06:02 -0400

    Created Slime Entity

---
+++ App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Entities/Slime.swift
@@ -0,0 +1,30 @@
+//
+//  Slime.swift
+//  ChaoticHotel-TvOS
+//
+//  Created by Renato Lopes on 14/02/19.
+//  Copyright © 2019 FF Studio. All rights reserved.
+//
+
+import SpriteKit
+import GameplayKit
+
+class Slime: GKEntity {
+    var stateMachine: GKStateMachine!
+    
+    init(withImageNamed name: String) {
+        super.init()
+        // Created instance components
+        let renderComponent = RenderComponent.init(imageNamed: name)
+        let moveComponent = MoveComponent.init()
+        
+        // Add components to Entity
+        self.addComponent(moveComponent)
+        self.addComponent(renderComponent)
+        
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
