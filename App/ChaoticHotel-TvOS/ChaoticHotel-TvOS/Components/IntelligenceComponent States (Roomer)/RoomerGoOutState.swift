81eb7b4959171c057ae473db66ddc3bf9a01e426
Victor Vasconcelos <victorabroum@gmail.com>
2019-02-14 08:36:55 -0400

    Put animation duration in relation of a constant

--- App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Components/IntelligenceComponent States (Roomer)/RoomerGoOutState.swift
+++ App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Components/IntelligenceComponent States (Roomer)/RoomerGoOutState.swift
@@ -35,8 +35,7 @@ class RoomerGoOutState: GKState {
         
         let spawnPosition = node.scene?.childNode(withName: "spawnRoomer")?.position
         
-        // TODO: #05 Observe duration on animate
-        node.run(SKAction.move(to: spawnPosition!, duration: 3)) {
+        node.run(SKAction.move(to: spawnPosition!, duration: AnimationDuration.roomerGoOut)) {
             node.removeFromParent()
         }
     }
