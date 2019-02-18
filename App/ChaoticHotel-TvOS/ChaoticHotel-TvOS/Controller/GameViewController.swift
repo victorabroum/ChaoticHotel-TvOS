c3f190d6952bdd00862b69a20338247f342502b8
Victor Vasconcelos <victorabroum@gmail.com>
2019-02-18 09:44:43 -0400

    Set Physics to visible

--- App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Controller/GameViewController.swift
+++ App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Controller/GameViewController.swift
@@ -41,6 +41,7 @@ class GameViewController: UIViewController {
 
                     view!.showsFPS = true
                     view!.showsNodeCount = true
+                    view!.showsPhysics = true
                 }
             }
         }
