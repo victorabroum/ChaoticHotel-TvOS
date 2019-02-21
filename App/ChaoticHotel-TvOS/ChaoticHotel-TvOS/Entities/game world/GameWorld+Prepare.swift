1b438cacf05d2e063281ace5e61e7fc356592cf3
Victor Vasconcelos <victorabroum@gmail.com>
2019-02-21 14:18:17 -0400

    Move Creation of Slime Entity to Game World

---
+++ App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Entities/game world/GameWorld+Prepare.swift
@@ -0,0 +1,37 @@
+//
+//  GameWorld+Prepare.swift
+//  ChaoticHotel-TvOS
+//
+//  Created by Victor Vasconcelos on 21/02/19.
+//  Copyright © 2019 FF Studio. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import GameplayKit
+
+extension GameWorld {
+    
+    func prepareLevel() {
+        self.prepareSlime()
+    }
+    
+    // Preapre Slime Enemy
+    func prepareSlime() {
+        let slime = Slime(
+            withImageNamed: "slime_placeHolder",
+            entityManager: self.scene.entityManager,
+            possibleSpawnArea: self.scene.childNode(withName: "slimeArea")!)
+        
+        guard let renderComponentSlime = slime.component(ofType: RenderComponent.self) else {return}
+        
+        let ySlime = self.scene.childNode(withName: "elevatorGoDown")?.position
+        
+        renderComponentSlime.node.position = CGPoint.init(
+            x: -(self.scene.size.width / 2) - 100,
+            y: ySlime!.y + 10)
+        
+        self.scene.entityManager.add(slime)
+        slime.crawlingInFloor()
+    }
+}
