00617d1dcd365b97c72a5911742effc25d87022b
Victor Vasconcelos <victorabroum@gmail.com>
2019-02-26 09:59:23 -0400

    Clean Animate

--- App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Settings/ZPositions.swift
+++ App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Settings/ZPositions.swift
@@ -13,7 +13,8 @@ struct ZPosition {
     static let starff: CGFloat = 10
     static let roomer: CGFloat = ZPosition.starff - 2
     static let items: CGFloat = ZPosition.starff + 2
-    static let slime: CGFloat = ZPosition.starff + 5
+    static let slime: CGFloat = ZPosition.starff + 6
+    static let goop: CGFloat = ZPosition.starff + 5
     
     // About Background
     static let bgPortal: CGFloat = 11
