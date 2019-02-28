c1b76353b2c81e1dea10992f15caed869c4fa409
Victor Vasconcelos <victorabroum@gmail.com>
2019-02-28 17:06:30 -0400

    Remove Cash label, count nodes and fps

--- App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Controller/GameViewController.swift
+++ App/ChaoticHotel-TvOS/ChaoticHotel-TvOS/Controller/GameViewController.swift
@@ -39,8 +39,8 @@ class GameViewController: UIViewController {
 
                     view!.ignoresSiblingOrder = true
 
-                    view!.showsFPS = true
-                    view!.showsNodeCount = true
+//                    view!.showsFPS = true
+//                    view!.showsNodeCount = true
 //                    view!.showsPhysics = true
                 }
             }
